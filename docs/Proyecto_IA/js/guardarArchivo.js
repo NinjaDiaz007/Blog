30b281f268a2bf3f4a6c08a780a35445eead7999
Alan Diaz <82740575+NinjaDiaz007@users.noreply.github.com>
2023-11-17 22:12:51 -0800

    Delete docs directory

--- docs/Proyecto_IA/js/guardarArchivo.js
+++
@@ -1,9 +0,0 @@
-const guardarArchivoDeTexto = (contenido, nombre) => {
-    const a = document.createElement("a");
-    const archivo = new Blob([contenido], { type: 'text/plain;charset=utf-8' });
-    const url = URL.createObjectURL(archivo);
-    a.href = url;
-    a.download = nombre;
-    a.click();
-    URL.revokeObjectURL(url);
-}
